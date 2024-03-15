91b9d948dc2a6446558f899b37e967c44e471d07
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-03-15 15:42:14 +0600

    db changed

--- database/daycare.sql
+++ database/daycare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 15, 2024 at 09:35 AM
+-- Generation Time: Mar 15, 2024 at 09:41 AM
 -- Server version: 5.7.32
 -- PHP Version: 8.0.3
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `daycare`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(9) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `name`, `email`, `username`, `password`) VALUES
+(1, 'Mymuna', 'mymuna.iubat@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -47,6 +68,12 @@ INSERT INTO `products` (`id`, `name`, `qty`, `price`, `login_id`) VALUES
 --
 
 --
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -56,6 +83,12 @@ ALTER TABLE `products`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
